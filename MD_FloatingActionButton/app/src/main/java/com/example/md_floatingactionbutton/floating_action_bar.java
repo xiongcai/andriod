03d3ea736843a642dc72eb0c9312a57d9bd5a1fe
xiongcai <2430939709@Dqq.com>
2017-01-06 16:05:17 +0800

    [floatingactionbutton v1.0]
    Signed-off-by: xiongcai <2430939709@Dqq.com>

---
+++ MD_FloatingActionButton/app/src/main/java/com/example/md_floatingactionbutton/floating_action_bar.java
@@ -0,0 +1,13 @@
+package com.example.md_floatingactionbutton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class floating_action_bar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_floating_action_bar);
+    }
+}
